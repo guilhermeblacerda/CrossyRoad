e4ab5bf72915f9506e1455567df6707f01eebd45
guilhermeblacerda <guilacerda1822@gmail.com>
2026-05-09 11:43:32 -0300

    criando a main

---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include "raylib.h"
+
+int main() {
+    
+    //tamanhos constantes de largura e altura da janela
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    //inicializa a janela com as dimensões já definidas
+    InitWindow(screenWidth, screenHeight, "Crossy Road");
+
+    //define as dimensões do player, no centro da tela
+    int playerX = 400;
+    int playerY = 300;
+    int playerSize = 40;
+
+    //60 fps por segundo
+    SetTargetFPS(60);
+
+
+    //jogo roda até fechar a janela ou ESC
+    while (!WindowShouldClose()) {
+
+        //W = cima
+        //S = baixo
+        //D = direita
+        //A = esquerda
+        if (IsKeyPressed(KEY_W)) playerY -= 40;
+        if (IsKeyPressed(KEY_S)) playerY += 40;
+        if (IsKeyPressed(KEY_A)) playerX -= 40;
+        if (IsKeyPressed(KEY_D)) playerX += 40;
+
+        //inicia o desenho
+        BeginDrawing();
+
+        //limpa a tela
+        ClearBackground(DARKGREEN);
+        
+        //desenha o jogador
+        DrawRectangle(playerX, playerY, playerSize, playerSize, YELLOW);
+
+        //finaliza o desenho do jogador
+        EndDrawing();
+    }
+
+    //finaliza o jogo
+    CloseWindow();
+
+    return 0;
+}
